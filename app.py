3e13338b7fe288e6fc6959b4bafd5533bf403825
ch-premchand <20r21a0412@mlrinstitutions.ac.in>
2023-10-24 19:16:54 +0530

    requirements added.

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from forms import SignUpForm, LoginForm
 from flask import session, redirect, url_for
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'dfewfew123213rwdsgert34tgfd1234trgf'
 
 """Information regarding the Pets in the System."""
 pets = [
@@ -34,7 +33,6 @@ def about():
 
 @app.route("/details/<int:pet_id>")
 def pet_details(pet_id):
-    """View function for Showing Details of Each Pet.""" 
     pet = next((pet for pet in pets if pet["id"] == pet_id), None) 
     if pet is None: 
         abort(404, description="No Pet was Found with the given ID")
@@ -43,7 +41,6 @@ def pet_details(pet_id):
 
 @app.route("/signup", methods=["POST", "GET"])
 def signup():
-    """View function for Showing Details of Each Pet.""" 
     form = SignUpForm()
     if form.validate_on_submit():
         new_user = {"id": len(users)+1, "full_name": form.full_name.data, "email": form.email.data, "password": form.password.data}
